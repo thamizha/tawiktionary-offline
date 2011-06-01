d825214151c56846ce528c325ec5135c37ec4445
tecoholic <arunthegy@gmail.com>
2011-06-01 15:39:25 +0530

    got searcher print hits

--- searcher.py
+++ searcher.py
@@ -5,3 +5,16 @@ The text entered during the searching should be parsed into search objects for t
 
 This file recieves the text entered and parses into searchable objects and performs search operations.
 '''
+from whoosh import index
+from whoosh.fields import *
+
+
+ix = index.open_dir("indexdir")
+
+
+from whoosh.qparser import QueryParser
+with ix.searcher() as searcher:
+    query = QueryParser("title", ix.schema).parse(u"ocean")
+    results = searcher.search(query, limit=None)
+    for result in results:
+        print result
--- splitter.py
+++ splitter.py
@@ -8,3 +8,23 @@ This script would split the large .bz2 file into smaller .bz2 files.
 
 Note: Make sure that page data isn't split between the smaller bits.
 '''
+
+#-------------------------------------------------------------------------
+'''
+Steps
+-----
+1. Set pages_per_file = 1000
+2. Open the large .xml.bz2 file
+3. parse the large file as such without decompression
+4. Read each page block and into a file buffer until count is 1000
+    or end of file
+5. Write the buffer as a file
+6. Repeat 4&5 until end of file
+
+'''
+#-------------------------------------------------------------------------
+
+import bz2
+
+ppfile = 20
+bzfile = bz2.BZ2File('wiki-files/tawiktionary-20110518-pages-articles.xml.bz2')
